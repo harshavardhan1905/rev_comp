38b85a467c40e380d2db26c13cad626b85810feb
harshavardhan1905 <podetiharshavardhan@gmail.com>
2025-12-15 16:08:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 
 // ---- ENABLE CORS BEFORE ROUTES ----
 app.use(cors({
-    origin: "https://sales.revuteck.in/",
+    origin: "https://sales.revuteck.in",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
