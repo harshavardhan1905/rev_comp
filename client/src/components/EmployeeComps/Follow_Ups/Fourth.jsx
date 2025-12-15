84a2ab9231a41aac156d16e068d6edd84a01cd44
harshavardhan1905 <podetiharshavardhan@gmail.com>
2025-12-15 12:02:45 +0530

    Update API endpoint for fetching candidates

--- client/src/components/EmployeeComps/Follow_Ups/Fourth.jsx
+++ client/src/components/EmployeeComps/Follow_Ups/Fourth.jsx
@@ -7,7 +7,7 @@ export default function Fourth() {
 
   useEffect(() => {
     axios
-      .get("http://localhost:5000/api/candidates")
+      .get("https://rev-comp-backend.onrender.com/api/candidates")
       .then((response) => {
         setCandidates(response.data);
       })
