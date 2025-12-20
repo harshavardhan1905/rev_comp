55828b79ff90924a31844b900b73575d37024262
harshavardhan1905 <podetiharshavardhan@gmail.com>
2025-12-20 10:49:44 +0530

    Update TodoList.jsx

--- client/src/components/AdminComps/TodoList.jsx
+++ client/src/components/AdminComps/TodoList.jsx
@@ -16,7 +16,7 @@ export default function TodoList() {
 
   const fetchCandidates = async () => {
     try {
-      const res = await axios.get("/api/candidates");
+      const res = await axios.get("https://rev-comp-backend.onrender.com/api/candidates");
       setCandidates(res.data);
     } catch (err) {
       console.error(err);
@@ -26,7 +26,7 @@ export default function TodoList() {
   /* -------------------- FETCH COUNTRIES -------------------- */
   useEffect(() => {
     axios
-      .get("/api/country/data")
+      .get("https://rev-comp-backend.onrender.com/api/country/data")
       .then((response) => setCountries(response.data))
       .catch((err) => console.log("Error fetching countries", err));
   }, []);
@@ -67,7 +67,7 @@ export default function TodoList() {
     }
 
     try {
-      await axios.put("/api/candidates/update-status", {
+      await axios.put("https://rev-comp-backend.onrender.com/api/candidates/update-status", {
         ids: selectedRows,
         stage,
       });
